e72742d0abef0dd0e06fc446231e104c7aeb77b6
Masper <casper.rang@gmail.com>
2018-09-28 15:38:21 +0200

    api af

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+/* import JSON-objects */
+
+var topicObject = require ('./data/topics');
+var messageObject = require ('./data/message');
+
+/* start server */
+
+app.listen(port, function()
+	{
+		console.log('Example app listening on port 3000!')
+	}
+	);
+
+
+app.get('/', function(req,res) {
+	res.send('Homepage is active!')
+}
+)
+
+app.get('/api/topics', function(req, res, next) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(topicObject)
+}
+)
+
+app.get('/api/message', function(req, res, next) {
+	res.setHeader('Content-Type', 'application/json');
+	res.json(messageObject);
+}
+)
